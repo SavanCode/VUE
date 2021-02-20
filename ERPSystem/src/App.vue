4ab27988bebc01ee68a9f0221879f0e9dd0be7a4
Savannah <2890513384@qq.com>
2021-02-20 16:19:39 +0800

    Update App.vue

--- ERPSystem/src/App.vue
+++ ERPSystem/src/App.vue
@@ -43,7 +43,6 @@ export default {
         // 页面开始加载时修改font-size
         var html = document.getElementsByTagName("html")[0];
         var oWidth = document.body.clientWidth || document.documentElement.clientWidth;
-        // 这里的750是指设计图的大小,自己根据实际情况改变
         html.style.fontSize = oWidth / 750 * 100 + "px";
         console.log('rem:', html.style.fontSize);
     }
