54c4faa5abe38e9b398068f0d0c61ca1c854ccc2
邬魉 <604675028@qq.com>
2021-02-22 10:29:07 +0800

    优化代码。

--- ERPSystem/src/App.vue
+++ ERPSystem/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app" :style="{ fontSize: fontSize +'px' }"> 
+  <div id="app" :style="{ fontSize: fontSize +'px' }">
     <!-- <router-view/> -->
-      <Nav :defaultSize="fontSize"/>
+    <Nav :defaultSize="fontSize"/>
     <div class="content">
       <left-bar @changed="setOptionSize"/>
       <paper-clip :optionSize="optionWidth"/>
@@ -11,96 +11,93 @@
 </template>
 
 <script>
-import Nav from '@/components/Nav'
-import LeftBar from '@/components/LeftBar'
-import Main from '@/components/Main'
-import PaperClip from '@/components/PaperClip'
+  import Nav from '@/components/Nav'
+  import LeftBar from '@/components/LeftBar'
+  import Main from '@/components/Main'
+  import PaperClip from '@/components/PaperClip'
 
-export default {
-  name: 'App',
-  components:{
-    Nav,
-    LeftBar,
-    Main,
-    PaperClip
-  }, 
-  data() {
-    return { 
-      fontSize: 0,
-      optionWidth:0,
-      window: {
+  export default {
+    name: 'App',
+    components: {
+      Nav,
+      LeftBar,
+      Main,
+      PaperClip
+    },
+    data() {
+      return {
+        fontSize: 0,
+        optionWidth: 0,
+        window: {
           width: 0,
-          height: 0, 
+          height: 0,
         }
-    };
-  },
-  provide () {
-    return {
-      window: this.window
-    }
-  },
-  created() {
-      window.addEventListener('resize', this.handleResize);
-      this.handleResize();
-  },
-  destroyed() {
-      window.removeEventListener('resize', this.handleResize);
-  },
-  methods:{
-    handleResize() {
+      };
+    },
+    provide() {
+      return {
+        window: this.window
+      }
+    },
+    created() {
       this.window.width = window.innerWidth;
-      this.window.height = window.innerHeight; 
+      this.window.height = window.innerHeight;
     },
-    setOptionSize:function(width){
-      console.log(width)
-      this.optionWidth=width
-    }
-  }, 
-  computed: {
-    getHeight(){
-      return this.window.height;
+    methods: {
+      setOptionSize: function (width) {
+        this.optionWidth = width
+      }
     },
-    getWidth(){
-      console.log(this)
-      return this.window.width;
-    }, 
-  },
-  watch: {
-        // getHeight(newval,oldval) {  },
-        getWidth(newval,oldval) {
-          //console.log(newval,oldval) 
-          if(newval=== oldval){
-              return
-          }
-          if(newval < 768){
-            this.fontSize = 10
-          }else if(768 < newval && newval<=1024){
-            this.fontSize = 15
-          }else if(newval > 1024){
-            this.fontSize = 30
-          }
+    computed: {
+      getHeight() {
+        return this.window.height;
+      },
+      getWidth() {
+        return this.window.width;
+      },
+    },
+    watch: {
+      getWidth(newval, oldval) {
+        if (newval === oldval) {
+          return
         }
+        if (newval < 768) {
+          this.fontSize = 10
+        } else if (768 < newval && newval <= 1024) {
+          this.fontSize = 15
+        } else if (newval > 1024) {
+          this.fontSize = 30
+        }
+      },
+      'window.innerWidth'(val) {
+        this.window.width = val;
+      },
+      'window.innerHeight'(val) {
+        this.window.height = val;
+      },
     }
-}
+  }
 </script>
 
 <style>
 
-#app { 
-  height: 100%;
-  min-width: 800px; /* 1024 */
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale; 
-  color: #2c3e50; 
-  background-color: #93aafd;
-  background-image: linear-gradient(270deg, #93aafd 0%, #ffffff 15%, #ffffff 85%, #93aafd 100%);
-}
-.content { 
-  display: flex;
-  flex-direction: row;
-}
-.main{
-  flex:1 0.5 auto
-}
+  #app {
+    height: 100%;
+    min-width: 800px; /* 1024 */
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    background-color: #93aafd;
+    background-image: linear-gradient(270deg, #93aafd 0%, #ffffff 15%, #ffffff 85%, #93aafd 100%);
+  }
+
+  .content {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .main {
+    flex: 1 0.5 auto
+  }
 </style>
--- ERPSystem/src/components/Btn.vue
+++ ERPSystem/src/components/Btn.vue
@@ -10,16 +10,16 @@
 export default {
   name:'Btn',
   inject: ['window'],
-   data () { 
+   data () {
     return {
       window: this.window
     }
   },
-  props: { 
+  props: {
       name: {
         type: String,
         default: 'defaultBtn',
-        required: true 
+        required: true
       },
       type:{
         type: String,
@@ -41,33 +41,31 @@ export default {
       optionSize:{
         type: Number,
         default: 20,
-        //required: true 
-      }, 
-        //这个可以用 但是想尝试inject 所以这个组件里就没用这个通过props传过来的 
+        //required: true
+      },
+        //这个可以用 但是想尝试inject 所以这个组件里就没用这个通过props传过来的
         //windowSize:{
         //   type: Number,
         //   default: 20,
-        //   required: true 
-        // } 
+        //   required: true
+        // }
     },
     computed:{
       btnWidth:function(){
-        //console.log("windowSize"+this.$parent._props.windowSize) 
-        //console.log(this.$root.$root.$el.clientWidth) 
         if(this.window.width<1200){
             return this.window.width/20
         }else{
           return this.window.width/10
-        } 
+        }
       },
       btnHeight:function(){
         if(this.window.width<600){
             return this.window.height/60
         }else{
           return this.window.height/20
-        } 
-      }, 
-      fontSize:function(){ 
+        }
+      },
+      fontSize:function(){
         return this.window.width/100
       }
     }
@@ -78,19 +76,19 @@ export default {
 .BtnSpace{
   margin:45px 0;
   display: flex;
-  align-items: center; 
+  align-items: center;
 }
 .blue{
-    background-color: rgb(130 225 255); 
+    background-color: rgb(130 225 255);
 }
 .yellow{
-    background-color: yellow; 
+    background-color: yellow;
 }
-.searchBtn{   
-    border-radius: 10px; 
+.searchBtn{
+    border-radius: 10px;
 }
 .blue, .yellow, .searchBtn{
-    border-style: solid; 
+    border-style: solid;
     min-height:30px;
     display: block;
     overflow: hidden;
@@ -117,7 +115,7 @@ export default {
   border-bottom-right-radius: 6px;
 }
 .blackBtn{
-  min-height: 30px;   
+  min-height: 30px;
   background-color: darkgray;
   display: flex;
   flex-direction: column-reverse;
