14d398e46912664d06093ebb0c02e21eba0443a0
Savannah <2890513384@qq.com>
2021-03-06 01:13:17 +0800

    update monitor router

--- dc3/src/App.vue
+++ dc3/src/App.vue
@@ -7,11 +7,16 @@
 <script>
     export default {
         name: "app",
-        // watch: {
-        //     "$router.name"(val) {
-        //         console.log(val)
-        //     }
-        // } 
+        watch: {
+             '$route' (to, from) { 
+              console.log('to.path----',to.path)//跳转后路由
+              console.log('from----',from)//跳转前路由 
+              if(Object.is(from.name,"login") &&  !Object.is(to.name,"home")){
+                    console.log("踢你一脚~")
+                    this.$router.replace('/login');
+              }
+            } 
+        } 
     };
 </script>
 <style lang="scss">
