462ad75d4bbd492ead8920aff06836f541fc2a50
Savannah <2890513384@qq.com>
2021-03-17 23:14:52 +0800

    Update main.js

--- mockjsDemo2/src/main.js
+++ mockjsDemo2/src/main.js
@@ -4,9 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import './mock/'
 import axios from 'axios'
+axios.defaults.baseURL=''
 
 Vue.config.productionTip = false
-Vue.prototype.$http = axios
+Vue.prototype.$http = axios//直接挂载给vue 主要是通过this.$http可以使用axios
 
 /* eslint-disable no-new */
 new Vue({
